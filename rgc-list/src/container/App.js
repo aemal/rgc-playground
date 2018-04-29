d22558a8f1f54d499aced92d5912e08ad0069c41
aemal <mudassar@reactgeeks.com>
2018-04-29 21:21:02 +0500

    Added space b/w ReactGeeks

--- rgc-list/src/container/App.js
+++ rgc-list/src/container/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>ReactGeeks</h1>
+        <h1>React Geeks</h1>
         <Lists todo={TodoData} rating={false}>
             {todo => todo.text}
         </Lists>
