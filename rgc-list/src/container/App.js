356ee81c6d4c1adeb4cd888edb3d23a4a22cde88
Aemal Sayer <mudassar@reactgeeks.com>
2018-04-29 21:17:27 +0500

    Changed title

--- rgc-list/src/container/App.js
+++ rgc-list/src/container/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, World</h1>
+        <h1>ReactGeek</h1>
         <Lists todo={TodoData} rating={false}>
             {todo => todo.text}
         </Lists>
